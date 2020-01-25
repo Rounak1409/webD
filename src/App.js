441b7002a0f4d5ee06ba19ce2c5f02e4fddc975d
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-01-25 13:34:20 +0800

    Changed path of dijkstra

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Dijkstra from './pages/graphs/Dijkstra';
-import GraphsPage from './pages/GraphsPage';
 
 function App() {
   return (
       <BrowserRouter>
           <Switch>
-              <Route exact path='/graphs' component={GraphsPage} />
-              <Route exact path='/graphs/dijkstra' component={Dijkstra} />
+              <Route exact path='/graphs' component={Dijkstra} />
           </Switch>
       </BrowserRouter>
   );
--- src/pages/GraphsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function GraphsPage(props) {
-  return <div>Graphs Page!!!!</div>;
-}
-
-export default GraphsPage;
