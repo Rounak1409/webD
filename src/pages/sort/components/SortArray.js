512ab655bdddcfadae523b7aa3fca3d69a60e8a7
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-02-08 22:31:31 +0800

    Added knuth shuffle algorithm for random permutation of array

--- src/pages/sort/components/SortArray.js
+++ src/pages/sort/components/SortArray.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-import {Slider, Button, InputNumber} from 'antd';
+import {Button, InputNumber} from 'antd';
 import Bar from './Bar';
 import './SortArray.css';
+import knuthShuffle from '../helpers/knuthShuffle';
 
 function SortArray(props) {
   // 0% - (100% - WIDTH%) width to divide bars
   // default width of 1 bar is 1.5%
   // height 0 - 65%
 
-  const [searchVal, setSearchVal] = useState(5);
   const initialRange = [];
   for (let i = 0; i < 10; i++) {
     initialRange.push({
@@ -17,15 +17,30 @@ function SortArray(props) {
       isOutOfRange: false,
     });
   }
+  knuthShuffle(initialRange);
   const [range, setRange] = useState(initialRange);
+  const [numMaxBars, setNumMaxBars] = useState(10);
+  const [inProgress, setInProgress] = useState(false); // for disabling buttons after clicking (eg while shuffling is being done)
   const helperDelay = ms => new Promise(res => setTimeout(res, ms));
 
   const reset = () => {
-    setSearchVal(5);
+    setNumMaxBars(10);
+    knuthShuffle(initialRange);
     setRange(initialRange);
   };
 
-  const renderBar = bar => {
+  const randomize = async () => {
+    const newRange = [];
+    for (let i = 0; i < range.length; i++) {
+      newRange.push(range[i]);
+    }
+    knuthShuffle(newRange);
+    setRange(newRange);
+    helperDelay(1000);
+  };
+
+  const renderBar = (bar, index) => {
+    console.log(bar);
     const numTotalBars = range.length;
 
     let width;
@@ -45,7 +60,7 @@ function SortArray(props) {
     const leftInterval = tolWidth / (numTotalBars - 1);
     const heightInterval = 100 / numTotalBars;
 
-    const left = (bar.val - 1) * leftInterval;
+    const left = index * leftInterval;
     const height = bar.val * heightInterval;
 
     return (
@@ -59,91 +74,55 @@ function SortArray(props) {
     );
   };
 
-  const binarySearch = async num => {
-    console.log(range);
-    let low = 1;
-    let high = range.length;
-    while (low <= high) {
-      console.log(`low: ${low} high: ${high}`);
-      let mid = Math.floor((low + high) / 2);
-      const temp = [];
-      for (let i = 0; i < range.length; i++) {
-        if (i + 1 === mid) {
-          temp.push({
-            val: i + 1,
-            isMid: true,
-            isOutOfRange: false,
-          });
-        } else if (i + 1 < low || i + 1 > high) {
-          temp.push({
-            val: i + 1,
-            isMid: false,
-            isOutOfRange: true,
-          });
-        } else {
-          temp.push({
-            val: i + 1,
-            isMid: false,
-            isOutOfRange: false,
-          });
-        }
-      }
-      setRange(temp);
-      if (mid === num) {
-        console.log(`Found ${num}!`);
-        break;
-      } else if (mid > num) {
-        // recurse on left
-        high = mid - 1;
-      } else {
-        // mid < num
-        // recurse on right
-        low = mid + 1;
-      }
-      await helperDelay(1500);
-    }
-  };
-
   return (
     <div style={{textAlign: 'center', height: '100%'}}>
       <h2>
-        Adjust total number of bars:
-        <Slider
-          onChange={e => {
+        Input total number of bars from 2 to 100:
+        <InputNumber
+          onChange={e => setNumMaxBars(e)}
+          size="large"
+          min={1}
+          max={100}
+          value={numMaxBars}
+        />
+        <Button
+          onClick={e => {
             let temp = [];
-            for (let i = 0; i < e; i++) {
+            for (let i = 0; i < numMaxBars; i++) {
               temp.push({
                 val: i + 1,
                 isMid: false,
                 isOutOfRange: false,
               });
             }
+            knuthShuffle(temp);
             setRange(temp);
-          }}
-          value={range.length}
-          min={2}
-          max={100}
-        />
-        Search Number from 1 to {range.length}:{' '}
-        <InputNumber
-          onChange={e => setSearchVal(e)}
-          size="large"
-          min={1}
-          max={range.length}
-          value={searchVal}
-        />
+          }}>
+          Submit
+        </Button>
+        <br />
+        Sort Number from 1 to {range.length}!
       </h2>
       <Button
-        onClick={e => binarySearch(searchVal)}
+        onClick={e => e}
         type="primary"
-        icon="search"
+        icon="play-circle"
         style={{marginBottom: '1em', marginRight: '1em'}}>
-        Search!
+        Sort!
+      </Button>
+      <Button
+        type="primary"
+        icon="redo"
+        onClick={e => randomize()}
+        style={{marginRight: '1em'}}>
+        Randomize
       </Button>
       <Button type="primary" icon="redo" onClick={e => reset()}>
         Reset
       </Button>
-      <div className="SortArray">{range.map(bar => renderBar(bar))}</div>
+      <div className="SortArray">
+        {range.map((bar, index) => renderBar(bar, index))}
+      </div>
     </div>
   );
 }
---
+++ src/pages/sort/helpers/knuthShuffle.js
@@ -0,0 +1,14 @@
+// generate random permutation of the barArr
+const knuthShuffle = barArr => {
+  for (let i = 1; i < barArr.length; i++) {
+    // range: 0 ... i (inclusive)
+    const range = i + 1;
+    const r = Math.floor(Math.random() * (range + 1));
+    // swap barArr[i] and barArr[r]
+    const temp = barArr[i];
+    barArr[i] = barArr[r];
+    barArr[r] = temp;
+  }
+};
+
+export default knuthShuffle;
