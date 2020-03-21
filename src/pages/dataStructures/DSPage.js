6c5710aca25b29f4f92fce594c2e837877abd3f0
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-03-22 00:23:15 +0800

    Changed default data structure to BST

--- src/pages/dataStructures/DSPage.js
+++ src/pages/dataStructures/DSPage.js
@@ -4,16 +4,19 @@ import BinarySearchTree from './components/BinarySearchTree';
 import AVLTree from './components/AVLTree';
 
 function DSPage(props) {
-  const [ds, setDs] = useState('AVL');
-    const {Option} = Select;
+  const [ds, setDs] = useState('BST');
+  const {Option} = Select;
   return (
     <div style={{textAlign: 'center', margin: '1em'}}>
-        <h1 style={{fontWeight: 'bold'}}>
+      <h1 style={{fontWeight: 'bold'}}>
         Choose the Data Structure:
-            <Select defaultValue={ds} onChange={e => setDs(e)} style={{margin: '0 1em', width: '12em'}}>
-                <Option value='BST'>Binary Search Tree</Option>
-                <Option value='AVL'>AVL Tree</Option>
-            </Select>
+        <Select
+          defaultValue={ds}
+          onChange={e => setDs(e)}
+          style={{margin: '0 1em', width: '12em'}}>
+          <Option value="BST">Binary Search Tree</Option>
+          <Option value="AVL">AVL Tree</Option>
+        </Select>
       </h1>
       <div>
         {ds === 'BST' && <BinarySearchTree />}
